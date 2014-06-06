a3306f6ec96e02f5ca9a4cc7212267c484927621
Timo Uhlmann <timouhlmann@googlemail.com>
2014-06-07 00:29:39 +0200

    yo backbone --test-framework=jasmine --template-framework=handlebars

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global WorldCupBracket, $*/
+
+
+window.WorldCupBracket = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    WorldCupBracket.init();
+});
