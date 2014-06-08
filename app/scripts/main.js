a575984460a001979a8ce7343e3746e633b6a074
Timo Uhlmann <t.uhlmann@zeroseven.de>
2014-06-08 10:04:13 +0200

    Stadium data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,82 @@
 /*global WorldCupBracket, $*/
 
+(function() {
+	var stadiums = null;
+	var wcb = window.WorldCupBracket = {
+		Models: {},
+		Collections: {},
+		Views: {},
+		Routers: {},
+		init: function() {
+			'use strict';
+			stadiums = window.stadiums = new wcb.Collections.Stadium();
+			stadiums.reset([
+				{
+					id: '01',
+					name: "Estádio Castelão",
+					city: "Fortaleza"
+				},
+				{
+					id: '02',
+					name: "Estádio das Dunas",
+					city: "Natal"
+				},
+				{
+					id: '03',
+					name: "Arena Pernambuco",
+					city: "Recife"
+				},
+				{
+					id: '04',
+					name: "Arena Fonte Nova",
+					city: "Salvador"
+				},
+				{
+					id: '05',
+					name: "Estádio Mineirão",
+					city: "Belo Horizonte"
+				},
+				{
+					id: '06',
+					name: "Estádio do Maracanã",
+					city: "Rio de Janeiro"
+				},
+				{
+					id: '07',
+					name: "Arena de São Paulo",
+					city: "São Paulo"
+				},
+				{
+					id: '08',
+					name: "Arena da Baixada",
+					city: "Curitiba"
+				},
+				{
+					id: '09',
+					name: "Estádio Beira-Rio",
+					city: "Porto Alegre"
+				},
+				{
+					id: '10',
+					name: "Estádio Nacional",
+					city: "Brasilia"
+				},
+				{
+					id: '11',
+					name: "Arena Pantanal",
+					city: "Cuiabá"
+				},
+				{
+					id: '12',
+					name: "Arena da Amazônia",
+					city: "Manaus"
+				}
+			]);
+		}
+	};
+})();
 
-window.WorldCupBracket = {
-    Models: {},
-    Collections: {},
-    Views: {},
-    Routers: {},
-    init: function () {
-        'use strict';
-        console.log('Hello from Backbone!');
-    }
-};
-
-$(document).ready(function () {
-    'use strict';
-    WorldCupBracket.init();
+$(document).ready(function() {
+	'use strict';
+	WorldCupBracket.init();
 });
