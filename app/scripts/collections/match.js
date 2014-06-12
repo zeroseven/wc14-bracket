f19c8f5bd752834272102f82a285e90a986b8411
Timo Uhlmann <t.uhlmann@zeroseven.de>
2014-06-12 10:15:26 +0200

    Knockout stage views

--- app/scripts/collections/match.js
+++ app/scripts/collections/match.js
@@ -5,27 +5,27 @@ WorldCupBracket.Collections = WorldCupBracket.Collections || {};
 (function() {
 	'use strict';
 
-	var Match = WorldCupBracket.Collections.Match = Backbone.Collection.extend({
+	WorldCupBracket.Collections.Match = Backbone.Collection.extend({
 
 		model: WorldCupBracket.Models.Match
 
 	});
 
 	WorldCupBracket.Collections.Match.bestOf16 = function() {
-		var collection = new Match();
-		[['a', 'b'], ['c', 'd'], ['e', 'f'], ['g', 'h']].forEach(function(teams) {
-			collection.add({
-				id: teams[0] + '-' + teams[1],
-				home: teams[0].toUpperCase() + '1',
-				guest: teams[1].toUpperCase() + '2'
-			});
-			collection.add({
-				id: teams[1] + '-' + teams[0],
-				home: teams[1].toUpperCase() + '1',
-				guest: teams[0].toUpperCase() + '2'
-			});
-		});
-		return collection;
+		// var collection = new Match();
+		// [['a', 'b'], ['c', 'd'], ['e', 'f'], ['g', 'h']].forEach(function(teams) {
+		// 	collection.add({
+		// 		id: teams[0] + '-' + teams[1],
+		// 		home: teams[0].toUpperCase() + '1',
+		// 		guest: teams[1].toUpperCase() + '2'
+		// 	});
+		// 	collection.add({
+		// 		id: teams[1] + '-' + teams[0],
+		// 		home: teams[1].toUpperCase() + '1',
+		// 		guest: teams[0].toUpperCase() + '2'
+		// 	});
+		// });
+		// return collection;
 	};
 
 })();
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,15 +7,85 @@
 
 	var accessor = function(name) {
 		return function(value) {
-			// console.log('accessor', this, name, value);
 			if(arguments.length === 0) {
-				// console.log('get', this.get(name));
 				return this.get(name);
 			} else {
-				// console.log('set', value);
 				this.set(name, value);
 			}
 		};
+	};
+
+	var createGroupMatches = function(data) {
+		var groupMatches = function(id, teams) {
+			var collection = new WorldCupBracket.Collections.Match();
+			data[id].forEach(function(match, index) {
+				collection.add({
+					id: id + index,
+					date: new Date(match[1])
+				},
+				{
+					home: teams[match[0][0]],
+					guest: teams[match[0][1]],
+					stadium: stadiums.get(match[2])
+				});
+			});
+			return collection;
+		};
+
+		// group stage
+		['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'].forEach(function(id, index) {
+			var groupTeams = teams.slice(index * 4, index * 4 + 4);
+			var group = new wcb.Models.Group({
+				id: id
+			}, {
+				teams: groupTeams,
+				matches: groupMatches(id, groupTeams)
+			});
+			groups.add(group);
+			var view = new wcb.Views.Group({
+				model: group,
+				el: $('.group--' + id.toLowerCase())
+			});
+			view.render();
+		});
+	};
+
+	var koId = function(round, index) {
+		if(round === 'finals') {
+			return index === 0 ? 'final' : 'consolation';
+		} else {
+			return ({
+				bestOf16: 'bo16',
+				quarterFinals: 'qf',
+				semiFinals: 'sf'
+			})[round] + '-' +(index + 1);
+		}
+	};
+
+	var createKnockoutMatches = function(round, data) {
+		var collection = new wcb.Collections.Match();
+		data.forEach(function(match, index) {
+			var id = koId(round, index);
+			var model = new wcb.Models.Match(
+				{
+					id: id,
+					date: new Date(match[1]),
+					home: match[0][0],
+					guest: match[0][1]
+				},
+				{
+					stadium: stadiums.get(match[2])
+				}
+			);
+			collection.add(model);
+
+			var view = new wcb.Views.KnockoutMatch({
+				id: 'AF ' + (index + 1),
+				model: model,
+				el: $('#' + id)
+			});
+			view.render();
+		});
 	};
 
 	var matchEvents = _.extend({}, Backbone.Events);
@@ -39,51 +109,22 @@
 
 			groups = new wcb.Collections.Group();
 
-			var groupMatches = function(id, teams) {
-				var collection = new WorldCupBracket.Collections.Match();
-				data.groupMatches[id].forEach(function(match, index) {
-					collection.add({
-						id: id + index,
-						date: new Date(match[1])
-					},
-					{
-						home: teams[match[0][0]],
-						guest: teams[match[0][1]],
-						stadium: stadiums.get(match[2])
-					});
-				});
-				return collection;
-			};
+			createGroupMatches(data.groupMatches);
 
-			// group stage
-			['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'].forEach(function(id, index) {
-				var groupTeams = teams.slice(index * 4, index * 4 + 4);
-				var group = new wcb.Models.Group({
-					id: id
-				}, {
-					teams: groupTeams,
-					matches: groupMatches(id, groupTeams)
-				});
-				groups.add(group);
-				var view = new wcb.Views.Group({
-					model: group,
-					el: $('.group--' + id.toLowerCase())
-				});
-				view.render();
-			});
+			_.pairs(data.knockoutMatches).forEach(function(args) {
+				createKnockoutMatches.apply(this, args);
+			}.bind(this));
 
-			// knockout stage
-			// best of 16
-			var bestOf16 = new wcb.Collections.Match.bestOf16();
+			// var bestOf16 = new wcb.Collections.Match.bestOf16();
 
-			bestOf16.each(function(match, index) {
-				var view = new wcb.Views.KnockoutMatch({
-					id: 'AF ' + (index + 1),
-					model: match,
-					el: $('.hexagon--' + match.id + ' .match')
-				});
-				view.render();
-			});
+			// bestOf16.each(function(match, index) {
+			// 	var view = new wcb.Views.KnockoutMatch({
+			// 		id: 'AF ' + (index + 1),
+			// 		model: match,
+			// 		el: $('.hexagon--' + match.id + ' .match')
+			// 	});
+			// 	view.render();
+			// });
 		}
 	};
 })();
